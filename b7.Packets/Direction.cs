fe7846a7317448349c32533941cf7e267af51602
b7c <58299468+b7c@users.noreply.github.com>
2021-04-06 03:14:39 +1200

    remove stuff

---
+++ b7.Packets/Direction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace b7.Packets.Common.Messages
+{
+    [Flags]
+    public enum Direction
+    {
+        Incoming = 1,
+        Outgoing = 2,
+        Both = Incoming | Outgoing
+    }
+}
